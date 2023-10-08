04b4d263435671127cbbd7b6f4f10a8d9f2ed221
Atsushi <atu729@icloud.com>
2023-10-08 22:34:17 +0900

    fix holder name

--- src/pages/components/AlertPage.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-type Props = {}
-
-const AlertPage = (props: Props) => {
-  return (
-    <div>AlertPage</div>
-  )
-}
--- src/pages/components/ButtonPage.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-type Props = {}
-
-const ButtonPage = (props: Props) => {
-  return (
-    <div>ButtonPage</div>
-  )
-}
