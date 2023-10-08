43ee49679d9d7ac44a3650ffa3afefff26a90008
Atsushi <atu729@icloud.com>
2023-10-08 22:39:53 +0900

    set redux and route

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import MainLayout from "./components/layout/MainLayout";
-import HomePage from "./pages/home/HomePage";
 import { routes } from "./routes";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter >
       <Routes>
         <Route path="/" element={<MainLayout />}>
           {routes}
