3db7de1e23e1bb2837134751e4b62a35407681e6
Atsushi <atu729@icloud.com>
2023-10-08 22:33:51 +0900

    update route

--- src/routes/appRoutes.tsx
+++ src/routes/appRoutes.tsx
@@ -4,16 +4,16 @@ import { RouteType } from "./config";
 import DefaultPage from "../pages/dashboard/DefaultPage";
 import DashboardIndex from "../pages/dashboard/DashboardIndex";
 import ChangelogPage from "../pages/changelog/ChangelogPage";
-// import AnalyticsPage from "../pages/dashboard/AnalyticsPage";
-// import SaasPage from "../pages/dashboard/SaasPage";
-// import ComponentPageLayout from "../pages/component/ComponentPageLayout";
+import AnalyticsPage from "../pages/dashboard/AnalyticsPage";
+import SaasPage from "../pages/dashboard/SaasPage";
+import ComponentPageLayout from "../pages/component/ComponentPageLayout";
 import DashboardOutlinedIcon from "@mui/icons-material/DashboardOutlined";
 import AppsOutlinedIcon from "@mui/icons-material/AppsOutlined";
 import ArticleOutlinedIcon from "@mui/icons-material/ArticleOutlined";
 import FormatListBulletedOutlinedIcon from "@mui/icons-material/FormatListBulletedOutlined";
 import FileDownloadOutlinedIcon from "@mui/icons-material/FileDownloadOutlined";
-// import AlertPage from "../pages/component/AlertPage";
-// import ButtonPage from "../pages/component/ButtonPage";
+import AlertPage from "../pages/component/AlertPage";
+import ButtonPage from "../pages/component/ButtonPage";
 // import InstallationPage from "../pages/installation/InstallationPage";
 // import DocumentationPage from "../pages/documentation/DocumentationPage";
 
@@ -23,6 +23,15 @@ const appRoutes: RouteType[] = [
     element: <HomePage />,
     state: "home",
   },
+  // {
+  //   path: "/installation",
+  //   element: <InstallationPage />,
+  //   state: "installation",
+  //   sidebarProps: {
+  //     displayText: "Installation",
+  //     icon: <FileDownloadOutlinedIcon />
+  //   }
+  // },
   {
     path: "/dashboard",
     element: <DashboardPageLayout />,
@@ -45,7 +54,68 @@ const appRoutes: RouteType[] = [
           displayText: "Default",
         },
       },
+      {
+        path: "/dashboard/analytics",
+        element: <AnalyticsPage />,
+        state: "dashboard.analytics",
+        sidebarProps: {
+          displayText: "Analytic",
+        },
+      },
+      {
+        path: "/dashboard/saas",
+        element: <SaasPage />,
+        state: "dashboard.saas",
+        sidebarProps: {
+          displayText: "Saas",
+        },
+      },
     ],
+  },
+  {
+    path: "/component",
+    element: <ComponentPageLayout />,
+    state: "component",
+    sidebarProps: {
+      displayText: "Components",
+      icon: <AppsOutlinedIcon />,
+    },
+    child: [
+      {
+        path: "/component/alert",
+        element: <AlertPage />,
+        state: "component.alert",
+        sidebarProps: {
+          displayText: "Alert",
+        },
+      },
+      {
+        path: "/component/button",
+        element: <ButtonPage />,
+        state: "component.button",
+        sidebarProps: {
+          displayText: "Button",
+        },
+      },
+    ],
+  },
+  {
+    path: "/documentation",
+    element: <ButtonPage />,
+    state: "documentation",
+    sidebarProps: {
+      displayText: "Documentation",
+      icon: <ArticleOutlinedIcon />,
+    },
+  },
+  {
+    path: "/changelog",
+    element: <ChangelogPage />,
+    state: "changelog",
+    sidebarProps: {
+      displayText: "Changelog",
+      icon: <FormatListBulletedOutlinedIcon />,
+    },
   },
 ];
 
