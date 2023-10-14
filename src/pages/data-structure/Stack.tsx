36c302a3235fb40a15c3010c24184d5dec9c5fae
Atsushi <atu729@icloud.com>
2023-10-14 23:56:30 +0900

    add stack description

--- src/pages/data-structure/Stack.tsx
+++ src/pages/data-structure/Stack.tsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import styled from "@emotion/styled";
+import { Typography } from "@mui/material";
+import CanvasComponent from "../../visualizer/data-structure/stack";
 
 const Stack = () => {
   return (
-    <div>Stack</div>
-  )
-}
+    <>
+      {/* Title */}
+      <Title variant="h3" gutterBottom>
+        Stack
+      </Title>
+      {/*  Card components */}
 
-export default Stack
+      {/* Text parts */}
+      <div>
+        In computer science, a stack is an abstract data type that serves as a
+        collection of elements, with two main operations.
+      </div>
+
+      <div>
+        The order in which an element added to or removed from a stack is
+        described as last in, first out, referred to by the acronym LIFO.[nb 1]
+        As with a stack of physical objects, this structure makes it easy to
+        take an item off the top of the stack, but accessing a datum deeper in
+        the stack may require taking off multiple other items first.
+      </div>
+      <CanvasComponent />
+    </>
+  );
+};
+
+//---------------------------------------------------------------------
+// Helpers
+//---------------------------------------------------------------------
+const Title = styled(Typography)({
+  display: "flex",
+  justifyContent: "center",
+  marginTop: "5%",
+  fontWeight: "bold",
+});
+
+export default Stack;
