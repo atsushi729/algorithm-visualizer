d33743afb308f74ac05a7463d81d55bdb8a6c8c2
Atsushi <atu729@icloud.com>
2023-10-19 23:50:31 +0900

    add card components

--- src/pages/data-structure/Stack.tsx
+++ src/pages/data-structure/Stack.tsx
@@ -1,42 +1,86 @@
 import styled from "@emotion/styled";
-import { Typography } from "@mui/material";
 import CanvasComponent from "../../visualizer/data-structure/stack";
 
 const Stack = () => {
   return (
-    <>
-      {/* Title */}
-      <Title variant="h3" gutterBottom>
-        Stack
-      </Title>
-      {/*  Card components */}
+    <Container>
+      <Title>Stack</Title>
+      <Subtitle>
+        FILO (first in last out) data structure
+      </Subtitle>
 
-      {/* Text parts */}
-      <div>
-        In computer science, a stack is an abstract data type that serves as a
-        collection of elements, with two main operations.
-      </div>
+      <Feature>
+        <Icon>🔒</Icon>
+        <FeatureTitle>Trusted content</FeatureTitle>
+        <FeatureDescription>
+          Track the life cycle of software artifacts built on trusted content,
+          including Docker Official Images, Docker Verified Publishers, and
+          Docker-Sponsored Open Source, which form the foundation for reliable
+          and secure software applications.
+        </FeatureDescription>
+      </Feature>
 
-      <div>
-        The order in which an element added to or removed from a stack is
-        described as last in, first out, referred to by the acronym LIFO.[nb 1]
-        As with a stack of physical objects, this structure makes it easy to
-        take an item off the top of the stack, but accessing a datum deeper in
-        the stack may require taking off multiple other items first.
-      </div>
+      <Feature>
+        <Icon>📊</Icon>
+        <FeatureTitle>Centralized view</FeatureTitle>
+        <FeatureDescription>
+          Operate from one view of centralized insights, for visibility and
+          control over all of the information in the software development
+          process from the software design phase into production.
+        </FeatureDescription>
+      </Feature>
+
+      <Feature>
+        <Icon>🔄</Icon>
+        <FeatureTitle>Recommended workflows</FeatureTitle>
+        <FeatureDescription>
+          Build faster and more reliable applications through context-aware
+          recommendations embedded into common developer workflows, all while
+          improving application security posture and ensuring compliance with
+          internal security requirements.
+        </FeatureDescription>
+      </Feature>
+      {/* data structure visualizer */}
       <CanvasComponent />
-    </>
+    </Container>
   );
 };
 
 //---------------------------------------------------------------------
 // Helpers
 //---------------------------------------------------------------------
-const Title = styled(Typography)({
-  display: "flex",
-  justifyContent: "center",
-  marginTop: "5%",
-  fontWeight: "bold",
+const Container = styled.div({
+  maxWidth: "800px",
+  margin: "0 auto",
+  fontFamily: "Arial, sans-serif",
+});
+
+const Title = styled.h1({
+  color: "black",
+});
+
+const Subtitle = styled.h2({
+  color: "black",
+});
+
+const Feature = styled.div({
+  backgroundColor: "#333",
+  color: "white",
+  padding: "20px",
+  margin: "20px 0",
+  borderRadius: "5px",
+});
+
+const Icon = styled.div({
+  fontSize: "24px",
+});
+
+const FeatureTitle = styled.h3({
+  color: "white",
+});
+
+const FeatureDescription = styled.p({
+  color: "white",
 });
 
 export default Stack;
