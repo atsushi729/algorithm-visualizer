ee020779c24ca8b25935681149709f3b271821b8
Atsushi <atu729@icloud.com>
2023-11-15 23:38:52 +0900

    refactor component

--- src/components/code/BasicCode.jsx
+++ src/components/code/BasicCode.jsx
@@ -5,34 +5,44 @@ import { useState } from "react";
 const Basic = ({ codeString = "", language = "javascript" }) => {
   const [copy, setCopy] = useState(false);
 
+  //---------------------------------------------------------------------
+  // Sub component
+  //---------------------------------------------------------------------
+  const CopiedButton = () => {
+    <button className="py-1 inline-flex items-center gap-1">
+      <span className="text-base mt-1">
+        <ion-icon name="checkmark-sharp"></ion-icon>
+      </span>
+      Copied
+    </button>;
+  };
+
+  const CopyButton = () => {
+    <button
+      className="py-1 inline-flex items-center gap-1"
+      onClick={() => {
+        navigator.clipboard.writeText(codeString);
+        setCopy(true);
+        setTimeout(() => {
+          setCopy(false);
+        }, 3000);
+      }}
+    >
+      <span className="text-base mt-1">
+        <ion-icon name="clipboard-outline"></ion-icon>
+      </span>
+      Copy code
+    </button>;
+  };
+
+  //---------------------------------------------------------------------
+  // Main component
+  //---------------------------------------------------------------------
   return (
     <div className="max-w-2x1 min-w-[25rem] bg-[#3a404d] rounded-md overflow-hidden">
       <div className="flex justify-between px-4 text-white text-xs items-center">
         <p className="text-sm">Example code</p>
-        {copy ? (
-          <button className="py-1 inline-flex items-center gap-1">
-            <span className="text-base mt-1">
-              <ion-icon name="checkmark-sharp"></ion-icon>
-            </span>
-            Copied
-          </button>
-        ) : (
-          <button
-            className="py-1 inline-flex items-center gap-1"
-            onClick={() => {
-              navigator.clipboard.writeText(codeString);
-              setCopy(true);
-              setTimeout(() => {
-                setCopy(false);
-              }, 3000);
-            }}
-          >
-            <span className="text-base mt-1">
-              <ion-icon name="clipboard-outline"></ion-icon>
-            </span>
-            Copy code
-          </button>
-        )}
+        {copy ? <CopiedButton /> : <CopyButton />}
       </div>
       <SyntaxHighlighter
         language={language}
