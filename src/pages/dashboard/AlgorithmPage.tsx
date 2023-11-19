eb4a34b8a2b39494fb6c193b639bc9ab45e42c33
Atsushi <atu729@icloud.com>
2023-11-19 23:11:15 +0900

    add example of algorithm

--- src/pages/dashboard/AlgorithmPage.tsx
+++ src/pages/dashboard/AlgorithmPage.tsx
@@ -19,7 +19,7 @@ const Algorithm = () => {
           <p className="text-base inline">
             Algorithm is consist of 3 elements.
           </p>
-          <ul className="list-decimal mb-2">
+          <ul className="list-decimal ps-5 space-y-2 mb-5 mt-3">
             <li>
               <strong>Input value </strong> : An algorithm has input values from
               a specified set.
@@ -33,8 +33,42 @@ const Algorithm = () => {
               algorithm produces output values from a specified set.
             </li>
           </ul>
+          <p>Here is a simple example that adds one to the input value:</p>
           <SyntaxHighlight codeString={code} language="javascript" />
           <h3 className="text-2xl mb-4 mt-4 font-bold">Sample of Algorithm</h3>
+          <ul className="marker:text-blue-600 list-disc ps-5 space-y-2">
+            <li>
+              <a
+                href="/dashboard/saas"
+                className="no-underline hover:underline font-bold"
+              >
+                Search Algorithm
+              </a>
+              <p>
+                A search algorithm is a step-by-step procedure used to locate
+                specific data among a collection of data. It is considered a
+                fundamental procedure in the field of computer science. There
+                are many types of search algorithms, such as Linear search,
+                Binary search.
+              </p>
+            </li>
+            <li>
+              <a
+                href="/dashboard/saas"
+                className="no-underline hover:underline font-bold"
+              >
+                Sort Algorithm
+              </a>
+              <p>
+                A sort algorithm is a method for arranging elements in lists or
+                sequences into a particular order. The most frequently desired
+                orders are numerical order and lexicographical order, and the
+                efficiency of an algorithm is determined by its performance in
+                terms of time and space complexity—specifically, how fast it can
+                sort and how much memory it requires.
+              </p>
+            </li>
+          </ul>
         </div>
       </div>
     );
