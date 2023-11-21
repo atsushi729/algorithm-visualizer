df9cdeb9e260289bee46de035c8bf669fcbf1327
Atsushi <atu729@icloud.com>
2023-11-21 23:33:29 +0900

    add expranation for data structure

--- src/pages/dashboard/AlgorithmPage.tsx
+++ src/pages/dashboard/AlgorithmPage.tsx
@@ -40,7 +40,7 @@ const Algorithm = () => {
             <li>
               <a
                 href="/dashboard/saas"
-                className="no-underline hover:underline font-bold"
+                className="no-underline hover:underline hover:text-blue-500 font-bold"
               >
                 Search Algorithm
               </a>
@@ -55,7 +55,7 @@ const Algorithm = () => {
             <li>
               <a
                 href="/dashboard/saas"
-                className="no-underline hover:underline font-bold"
+                className="no-underline hover:underline hover:text-blue-500 font-bold"
               >
                 Sort Algorithm
               </a>
@@ -74,7 +74,75 @@ const Algorithm = () => {
     );
   };
 
-  const Pseudocode = () => {
+  const AboutDataStructure = () => {
+    return (
+      <div className="flex">
+        <div className="max-w-4xl m-auto p-8">
+          <h1 className="text-3xl mb-4 font-bold">Data Structure</h1>
+          <p className="text-lg mb-4">
+            Data Structure is a particular way of organizing data in a computer
+            so that it can be used effectively. Common examples of data
+            structures include arrays, linked lists, stacks, queues, trees, and
+            graphs. The choice of a data structure often depends on the specific
+            needs of the algorithm or application, such as how data is accessed,
+            manipulated, and stored. Efficient data structures can significantly
+            improve the performance of an algorithm.
+          </p>
+          <h5 className="text-lg mb-4 mt-4 font-bold">
+            Example of Data Structure:
+          </h5>
+          <ul className="marker:text-blue-600 list-disc ps-5 space-y-2">
+            <li>
+              <a
+                href="/data-structure/stack"
+                className="no-underline hover:underline hover:text-blue-500 font-bold"
+              >
+                Stack
+              </a>
+              <p>
+                A stack is a linear data structure that follows the Last In,
+                First Out (LIFO) principle. The last element added to the stack
+                will be the first one to be removed. It's analogous to a stack
+                of plates.
+              </p>
+            </li>
+            <li>
+              <a
+                href="/data-structure/queue"
+                className="no-underline hover:underline hover:text-blue-500 font-bold"
+              >
+                Queue
+              </a>
+              <p>
+                A queue is a linear data structure that follows the First In,
+                First Out (FIFO) principle. The first element added will be the
+                first one to be removed. This is similar to a queue of people
+                waiting in line; the first person in line is the first to be
+                served.
+              </p>
+            </li>
+            <li>
+              <a
+                href="/data-structure/lined-list"
+                className="no-underline hover:underline hover:text-blue-500 font-bold"
+              >
+                Linkd list
+              </a>
+              <p>
+                A linked list is a linear data structure where each element
+                (commonly called a node) contains a reference (or link) to the
+                next node in the sequence. In a singly linked list, each node
+                points to the next node. In a doubly linked list, each node has
+                two links, one to the next node and one to the previous node.
+              </p>
+            </li>
+          </ul>
+        </div>
+      </div>
+    );
+  };
+
+  const AboutPseudocode = () => {
     return (
       <div className="flex">
         <div className="max-w-4xl m-auto p-8">
@@ -87,12 +155,12 @@ const Algorithm = () => {
           </p>
           <p>Here is a simple example that adds one to the input value:</p>
           <SyntaxHighlight codeString={pseudocode} language="pseudocode" />
-          <h5 className="text-lg mb-4 mt-4 font-bold">Sample of Algorithm</h5>
+          <h5 className="text-lg mb-4 mt-4 font-bold">
+            Benefits of Pseudocode
+          </h5>
           <ul className="marker:text-blue-600 list-decimal ps-5 space-y-2">
             <li>
-              <p className="no-underline hover:underline font-bold">
-                Algorithm Representation
-              </p>
+              <p className="no-underline font-bold">Algorithm Representation</p>
               <p>
                 Pseudocode provides a way to represent an algorithm's logic in a
                 format that is easier to understand than actual code. It focuses
@@ -102,12 +170,7 @@ const Algorithm = () => {
               </p>
             </li>
             <li>
-              <a
-                href="/dashboard/saas"
-                className="no-underline hover:underline font-bold"
-              >
-                Language Independence
-              </a>
+              <p className="no-underline font-bold">Language Independence</p>
               <p>
                 Pseudocode is not tied to any specific programming language. It
                 can be understood by programmers who are familiar with different
@@ -116,12 +179,9 @@ const Algorithm = () => {
               </p>
             </li>
             <li>
-              <a
-                href="/dashboard/saas"
-                className="no-underline hover:underline font-bold"
-              >
+              <p className="no-underline font-bold">
                 Simplifying Complex Logic
-              </a>
+              </p>
               <p>
                 It helps in breaking down complex algorithmic logic into
                 simpler, more manageable steps. This makes it easier to
@@ -140,7 +200,8 @@ const Algorithm = () => {
   return (
     <>
       <AboutAlgorithm />
-      <Pseudocode />
+      <AboutDataStructure />
+      <AboutPseudocode />
     </>
   );
 };
