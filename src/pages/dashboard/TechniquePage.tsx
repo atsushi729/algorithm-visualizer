9d49e3961f521a709dacae173f22f0cd0934fc97
Atsushi <atu729@icloud.com>
2023-11-29 23:50:57 +0900

    add text

--- src/pages/dashboard/TechniquePage.tsx
+++ src/pages/dashboard/TechniquePage.tsx
@@ -1,7 +1,30 @@
 type Props = {};
 
 const TechniquePage = (props: Props) => {
-  return <div>TechniquePage</div>;
+  //---------------------------------------------------------------------
+  // Sub component
+  //---------------------------------------------------------------------
+  const AboutTimeComplexity = () => {
+    return (
+      <div className="flex">
+        <div className="max-w-4xl m-auto p-8">
+          <h1 className="text-3xl mb-4 mt-4 font-bold">Time complexity</h1>
+          <p className="text-lg mb-4">
+            Time complexity is a concept in computer science that describes the
+            amount of computer time it takes to run an algorithm. Time
+            complexity is usually expressed using Big O notation, which helps to
+            quantify how the runtime of an algorithm increases with the size of
+            the input data set.
+          </p>
+          <p className="text-base inline">
+            Here's a brief explanation of common time complexity terms:
+          </p>
+        </div>
+      </div>
+    );
+  };
+
+  return <AboutTimeComplexity />;
 };
 
 export default TechniquePage;
