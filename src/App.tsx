52214712b5130df4a406baa01b7d0dd456abeb07
Atsushi <atu729@icloud.com>
2023-12-10 16:32:39 +0900

    fix scroll issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
 import MainLayout from "./components/layout/MainLayout";
 import { routes } from "./routes";
+import { useLayoutEffect } from "react";
+
+function ScrollToTop() {
+  const location = useLocation();
+
+  useLayoutEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location.pathname]);
+
+  return null;
+}
 
 function App() {
   return (
-    <BrowserRouter >
+    <BrowserRouter>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<MainLayout />}>
           {routes}
