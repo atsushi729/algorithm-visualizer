8a43efbfb7995693f2542cf7a063a308d6f68228
Atsushi <atu729@icloud.com>
2024-01-12 23:40:12 +0900

    remove comment out

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -3,13 +3,12 @@ import { Box, Toolbar, styled } from "@mui/material";
 import colorConfigs from "../../configs/colorConfigs";
 import sizeConfigs from "../../configs/sizeConfigs";
 import Sidebar from "../common/Sidebar";
-import React from 'react';
-// import Topbar from "../common/Topbar";
+import Topbar from "../common/Topbar";
 
 const MainLayout = () => {
   return (
     <Container>
-      {/* <Topbar /> */}
+      <Topbar />
       <NavigationBox component="nav">
         <Sidebar />
       </NavigationBox>
