0f06499335f20b4162c7e9d99230ab321069a132
Atsushi <atu729@icloud.com>
2024-02-21 23:10:19 +0900

    add sample reference data

---
+++ src/constants/reference/reference.ts
@@ -0,0 +1,10 @@
+export const references = [
+  {
+    author: "Smith, J.",
+    year: "2020",
+    title: "An overview of React for web development",
+    journal: "Journal of Web Development",
+    volume: "15",
+    pages: "112-123",
+  },
+];
