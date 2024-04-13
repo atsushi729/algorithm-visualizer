64c71e8e8e924a92c5ac673f5510c87580d9c63b
Atsushi <atu729@icloud.com>
2024-04-13 23:24:17 +0900

    add reference

--- src/constants/reference/reference.ts
+++ src/constants/reference/reference.ts
@@ -1,3 +1,4 @@
+// prettier-ignore
 export const references = [
   {
     author: "Smith, J.",
@@ -6,5 +7,21 @@ export const references = [
     journal: "Journal of Web Development",
     volume: "15",
     pages: "112-123",
+  },
+  {
+    author: "Thomas H. Cormen, Charles E. Leiserson, Ronald L. Rivest, Clifford Stein",
+    year: "2009",
+    title: "Introduction to Algorithms, 3rd Edition (MIT Press)",
+    journal: "",
+    volume: "3",
+    pages: "",
+  },
+  {
+    author: "Gayle Laakmann McDowell",
+    year: "2015",
+    title: "Cracking the Coding Interview: 189 Programming Questions and Solutions",
+    journal: "",
+    volume: "6",
+    pages: "",
   },
 ];
