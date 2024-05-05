7f4ca35ac204549b3fb8ea54cbc0a2a18218dca5
Atsushi <atu729@icloud.com>
2024-05-05 14:17:00 +0900

    add anchorlinks

--- src/constants/common/anchorLink.ts
+++ src/constants/common/anchorLink.ts
@@ -27,30 +27,35 @@ export const arrayAnchorLinks = [
   { href: "#array", label: "About Array" },
   { href: "#operation", label: "Operation" },
   { href: "#playground", label: "Playground" },
+  { href: "#vizualization", label: "Visualization" },
 ];
 
 export const stackAnchorLinks = [
   { href: "#stack", label: "About Stack" },
   { href: "#operation", label: "Operation" },
   { href: "#playground", label: "Playground" },
+  { href: "#vizualization", label: "Visualization" },
 ];
 
 export const queueAnchorLinks = [
   { href: "#queue", label: "About Queue" },
   { href: "#operation", label: "Operation" },
   { href: "#playground", label: "Playground" },
+  { href: "#vizualization", label: "Visualization" },
 ];
 
 export const linkedListAnchorLinks = [
   { href: "#linked-list", label: "About Linked list" },
   { href: "#operation", label: "Operation" },
   { href: "#playground", label: "Playground" },
+  { href: "#vizualization", label: "Visualization" },
 ];
 
 export const treeAnchorLinks = [
   { href: "#tree", label: "About Tree" },
   { href: "#operation", label: "Operation" },
   { href: "#playground", label: "Playground" },
+  { href: "#vizualization", label: "Visualization" },
 ];
 
 //---------------------------------------------------------------------
